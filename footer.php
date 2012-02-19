c5d8ec1f959d6ef5ccf1ba0e802b55990ca424eb
Alex Palcuie <alex.palcuie@gmail.com>
2012-02-19 17:54:22 +0200

    added last modification in footer

--- footer.php
+++ footer.php
@@ -1,6 +1,10 @@
 	</div>
 	<div id="footer"
   <p>&copy 2011 - <a href="<?=MAIN_HOME_PAGE?>"><?=TITLE?></a> - Realizat de <a href="http://palcu.blogspot.com/">Alexandru Palcuie</a></p>
+  
+  <p>
+    <i>Ultima modificare <?=lastModified()?></i>
+  </p>
 	</div>
 	</body>
 </html>
