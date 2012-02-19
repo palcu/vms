0b84a24526e81b7fb30580c249d74269d3993312
Alex Palcuie <alex.palcuie@gmail.com>
2012-02-19 17:57:17 +0200

    added github link to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 	</div>
 	<div id="footer"
-  <p>&copy 2011 - <a href="<?=MAIN_HOME_PAGE?>"><?=TITLE?></a> - Realizat de <a href="http://palcu.blogspot.com/">Alexandru Palcuie</a></p>
+  <p>&copy 2012 - <a href="<?=MAIN_HOME_PAGE?>"><?=TITLE?></a> - Realizat de <a href="http://palcu.blogspot.com/">Alexandru Palcuie</a> - Open Source pe <a href="https://github.com/palcu/vms">Github</a></p>
   
   <p>
     <i>Ultima modificare <?=lastModified()?></i>
